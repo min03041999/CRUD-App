4491de8bfd138df1913106b9aa835adce406d641
Min Kookie <min03041999@gmail.com>
2022-05-14 11:22:55 +0700

    feat(navbar): add navbar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,52 @@ import React from "react";
 import { Nav, Navbar, Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
+// Import from react-router-dom
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
 import CreateStudent from "./components/create-student.component";
+import StudentList from "./components/student-list.component";
 
 function App() {
   return (
-    <div>
-      <CreateStudent />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to={"/create-student"} className="nav-link">
+                  React Mern Stack App
+                </Link>
+              </Navbar.Brand>
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={"/create-student"} className="nav-link">
+                    CreateStudent
+                  </Link>
+                </Nav>
+                <Nav>
+                  <Link to={"/student-list"} className="nav-link">
+                    Student List
+                  </Link>
+                </Nav>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+
+        <Container>
+          <Row>
+            <Col md={12}>
+              <Route exact path="/" component={CreateStudent} />
+              <Route path="/create-student" component={CreateStudent} />
+              <Route path="/student-list" component={StudentList} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
